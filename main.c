ab3ab60842cef1df9f11676c1b94787577e50716
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-05-17 12:35:44 +0200

    idk

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
   PCA9685 pca = pca_new("/dev/i2c-1", 0x40);
   pca_set_pwm_freq(&pca, 50);
   while (1) {
-    pca_set_pwm_ms(pca, 0, 2);
+    pca_set_pwm_ms(pca, 0, 3);
     delay(500);
     pca_set_pwm_ms(pca, 0, 1);
     delay(500);
