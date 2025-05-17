f409435affd4b76c0a570e63ee1bdd5014073c09
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-05-17 10:29:04 +0200

    idk

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "wiringPi.h"
+#include <stdio.h>
 
 int main() {
   wiringPiSetupGpio();
   digitalWrite(23, HIGH);
+  printf("HIGH\n");
   delay(1000);
   digitalWrite(23, LOW);
+  printf("LOW\n");
 }
