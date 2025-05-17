a323843494ed8429335cfbeafd9de8e278452b07
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-05-17 13:09:20 +0200

    idk

--- main.c
+++ main.c
@@ -11,18 +11,18 @@ int main() {
   PCA9685 pca = pca_new("/dev/i2c-1", 0x40);
   pca_set_pwm_freq(&pca, 50);
 
-  // as5600_t sensor;
-  // if (as5600_init("/dev/i2c-2", &sensor) != 0) {
-  //   perror("as5600 init");
-  //   exit(1);
-  // }
+  as5600_t sensor;
+  if (as5600_init("/dev/i2c-1", &sensor) != 0) {
+    perror("as5600 init");
+    exit(1);
+  }
 
 
   while (1) {
     double ms = .5 + (sin((double)micros() / 200000.) + 1.) / 2. * 2.;
     pca_set_pwm_ms(pca, 0, ms);
 
-    // uint16_t angle = as5600_read_angl(&sensor);
-    // printf("%d\n", (int)angle);
+    uint16_t angle = as5600_read_angl(&sensor);
+    printf("%d\n", (int)angle);
   }
 }
