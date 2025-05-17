72da31c92d2a54466a33edd6a35109e68d979929
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-05-17 12:38:59 +0200

    idk

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "wiringPi.h"
 #include "i2cp.h"
+#include <math.h>
 
 int main() {
   wiringPiSetupGpio();
   PCA9685 pca = pca_new("/dev/i2c-1", 0x40);
   pca_set_pwm_freq(&pca, 50);
+
   while (1) {
-    pca_set_pwm_ms(pca, 0, 2);
-    delay(500);
-    pca_set_pwm_ms(pca, 0, 1);
-    delay(500);
+    pca_set_pwm_ms(pca, 0, 1 + sin((double)micros() / 1000000.));
+    delay(10);
   }
 }
